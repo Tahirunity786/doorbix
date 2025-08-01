e07490eeffbb4c4516f3b7e4f8043db90cbd375d
Tahir Zaman <tahirunity786@gmail.com>
2025-08-01 22:00:16 +0500

    Server update 3

--- processor/settings.py
+++ processor/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -53,7 +54,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'processor.urls'
 AUTH_USER_MODEL = 'core_a.CustomUser'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
