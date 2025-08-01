533ebc0735d1e835e94ad659ccbf852badeeb679
Tahir <tahirunity786@gmail.com>
2025-08-01 16:54:34 +0000

    Updated server 2

--- processor/settings.py
+++ processor/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-upkrivw6ipv1q)63-i)f!m3g7(+wnv7ciq7y67l!$ij677&=mj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -53,7 +55,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'processor.urls'
 AUTH_USER_MODEL = 'core_a.CustomUser'
-
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
